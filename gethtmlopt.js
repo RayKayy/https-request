eba67f51c2cab3054580c983f8a65f5ff558b0a9
Raymond Kwan <rayk199492@gmail.com>
2018-09-03 15:22:25 +0000

    Added gethtmlopt.js to implement a function to take in options to request from url within opts

---
+++ gethtmlopt.js
@@ -0,0 +1,26 @@
+const https = require('https');
+
+function getAndPrintHTML(requestOptions) {
+  const buffer = [];
+
+  /* Add your code here */
+  https.get(requestOptions, (res) => {
+    res.setEncoding('utf-8');
+
+    res.on('data', (chunk) => {
+      buffer.push(chunk);
+      console.log('pushed chunk to buffer');
+    });
+    res.on('end', () => {
+      console.log('stream ended');
+      console.log(buffer.join(''));
+    });
+  });
+}
+
+const requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html',
+};
+
+getAndPrintHTML(requestOptions);
